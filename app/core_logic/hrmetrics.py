4c3c4cc6fb9d2d5efad8160cb1973e940631d52b
abhijay <abhijay@tententen.in>
2018-03-21 09:06:27 +0530

    minor changes js done

--- app/core_logic/hrmetrics.py
+++ app/core_logic/hrmetrics.py
@@ -29,8 +29,8 @@ def dashboard(obj):
 @hrmetrics.route('/',methods=['GET'])
 @user_access()
 def employee_cost(obj):
-    resp = obj.user_averages_rs(required_field="Employee_Cost",json=1)
-    return resp
+    resp = obj.user_averages_rs(required_field="Employee_Cost",json=0)
+    resp["data"].update(dict(head="Employee Cost (INR Crore or $)"))
     return render_template('benchmark_results.html',resp=resp["data"])
 
 
@@ -38,6 +38,7 @@ def employee_cost(obj):
 @user_access()
 def Revenue_Per_Employee(obj):
     resp = obj.user_averages_rs(required_field="Revenue_Per_Employe",json=0)
+    resp["data"].update(dict(head="Revenue Per Employe ($)"))
     return render_template('benchmark_results.html',resp=resp["data"])
 
 
@@ -45,6 +46,7 @@ def Revenue_Per_Employee(obj):
 @user_access()
 def Employee_Cost_Revenue_percentage(obj):
     resp = obj.user_averages_rs(required_field="Employee_Cost_Revenue_percentage",json=0)
+    resp["data"].update(dict(head="Employee Cost as % of Revenue"))
     return render_template('benchmark_results.html',resp=resp["data"])
 
 
@@ -52,6 +54,7 @@ def Employee_Cost_Revenue_percentage(obj):
 @user_access()
 def Average_Employee_Cost(obj):
     resp = obj.user_averages_rs(required_field="Average_Employee_Cost_rs",json=0)
+    resp["data"].update(dict(head="Average Employee Cost (INR Crore or $)"))
     return render_template('benchmark_results.html',resp=resp["data"])
 
 
@@ -62,6 +65,7 @@ def Average_Employee_Cost(obj):
 @user_access()
 def Voluntary_Attrition_Annual(obj):
     resp = obj.user_averages_rs(required_field="Voluntary_Attrition_Annual",json=0)
+    resp["data"].update(dict(head="Employee Cost (INR Crore or $)"))
     return render_template('benchmark_results.html',resp=resp["data"])
 
 
@@ -70,6 +74,7 @@ def Voluntary_Attrition_Annual(obj):
 @user_access()
 def attrition_overall(obj):
     resp = obj.user_averages_rs(required_field="Overall_Attrition_Annual",json=0)
+    resp["data"].update(dict(head="EmployeeVoluntary Attrition - Annual"))
     return render_template('benchmark_results.html',resp=resp["data"])
 
 
@@ -78,6 +83,7 @@ def attrition_overall(obj):
 @user_access()
 def Average_Hiring_Quarterly(obj):
     resp = obj.user_averages_rs(required_field="Average_Hiring_Quarterly",json=0)
+    resp["data"].update(dict(head="Average Hiring"))
     return render_template('benchmark_results.html',resp=resp["data"])
 
 
@@ -85,6 +91,7 @@ def Average_Hiring_Quarterly(obj):
 @user_access()
 def Average_Hiring_Annual(obj):
     resp = obj.user_averages_rs(required_field="Average_Hiring_Annual",json=0)
+    resp["data"].update(dict(head="Average Hiring (Annual)"))
     return render_template('benchmark_results.html',resp=resp["data"])
 
 
@@ -92,6 +99,7 @@ def Average_Hiring_Annual(obj):
 @user_access()
 def Cost_Per_Hire_Annual(obj):
     resp = obj.user_averages_rs(required_field="Cost_Per_Hire_Annual",json=0)
+    resp["data"].update(dict(head="Employee Cost (INR Crore or $)"))
     return render_template('benchmark_results.html',resp=resp["data"])
 
 
@@ -101,6 +109,7 @@ def Cost_Per_Hire_Annual(obj):
 @user_access()
 def Time_to_Hire_Days(obj):
     resp = obj.user_averages_rs(required_field="Time_to_Hire_Days",json=0)
+    resp["data"].update(dict(head="Employee Cost (INR Crore or $)"))
     return render_template('benchmark_results.html',resp=resp["data"])
 
 
@@ -111,22 +120,26 @@ def Time_to_Hire_Days(obj):
 @user_access()
 def HR_Headcount(obj):
     resp = obj.user_averages_rs(required_field="HR_Headcount",json=0)
+    resp["data"].update(dict(head="Employee Cost (INR Crore or $)"))
     return render_template('benchmark_results.html',resp=resp["data"])
 
 @hrmetrics.route('/employee_to_hr',methods=['GET'])
 @user_access()
 def Employee_to_HR(obj):
     resp = obj.user_averages_rs(required_field="Employee_to_HR",json=0)
+    resp["data"].update(dict(head="Employee Cost (INR Crore or $)"))
     return render_template('benchmark_results.html',resp=resp["data"])
 
 @hrmetrics.route('/hr_bp_headcount',methods=['GET'])
 @user_access()
 def HR_BP_Headcount(obj):
     resp = obj.user_averages_rs(required_field="HR_BP_Headcount",json=0)
+    resp["data"].update(dict(head="Employee Cost (INR Crore or $)"))
     return render_template('benchmark_results.html',resp=resp["data"])
 
 @hrmetrics.route('/ratio_employee_hrbp',methods=['GET'])
 @user_access()
 def Employee_HR_BP_Ratio(obj):
     resp = obj.user_averages_rs(required_field="Employee_HR_BP_Ratio",json=0)
+    resp["data"].update(dict(head="Employee Cost (INR Crore or $)"))
     return render_template('benchmark_results.html',resp=resp["data"])
