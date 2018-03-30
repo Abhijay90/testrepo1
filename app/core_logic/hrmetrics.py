5f1794fafce4bbbd91290f2c4b4ecdadeea05d78
abhijay <abhijay@tententen.in>
2018-03-30 17:27:55 +0530

    list fixes

--- app/core_logic/hrmetrics.py
+++ app/core_logic/hrmetrics.py
@@ -178,17 +178,18 @@ def ger_resp_head(key):
     "Employee_to_HR":"Employee to HR Ratio",
     "HR_Headcount":"HR Headcount",
     "Time_to_Hire_Days":"Time to Hire (Days)",
-    "Cost_Per_Hire_Annual":"Cost Per Hiring Annual",
+    "Cost_Per_Hire_Annual":"Cost Per Hiring Annual($)",
     "Average_Hiring_Annual":"Average Hiring (Annual)",
     "Average_Hiring_Quarterly":"Average Hiring",
-    "Overall_Attrition_Annual":"Employee Voluntary Attrition - Annual",
-    "Voluntary_Attrition_Annual":"Employee Cost ($)",
-    "Employee_Cost":"Employee Cost (INR Crore or $)",
+    "Overall_Attrition_Annual":"Employee Overall Attrition (Annual)",
+    "Voluntary_Attrition_Annual":"Employee Voluntary Attrition (Annual)",
+    "Employee_Cost":"Aerage Employee Cost ($)",
     "Revenue_Per_Employe":"Revenue Per Employee ($)",
     "Employee_Cost_Revenue_percentage":"Employee Cost as % of Revenue",
-    "Average_Employee_Cost_rs":"Avg Employee Cost ($)",
+    "Average_Employee_Cost_rs":"Aerage Employee Cost ( Rs. )",
     "Training_Spend_Annual":"Training Spend (Annual)",
     "Training_Spend_Revenue_Percentage":"Training Spend as % of Revenue",
+    "Training_Spend_Per_Person":"Training Spend Per Person ($)"
     }
 
     return headings[key]
